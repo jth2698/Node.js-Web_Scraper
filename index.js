ae7e8dfe14f763fdf354b2d31c2b18586f3ff1fe
Eric D. Torres <etorresnotary@gmail.com>
2020-11-24 08:20:42 -0600

    selecting elements with css id

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const cheerio = require("cheerio");
 
 async function main() {
   const html = await request.get(
-    "https://reactnativetutorial.net/css-selectors/lesson2.html"
+    "https://reactnativetutorial.net/css-selectors/lesson3.html"
   );
+
   fs.writeFileSync("./test.html", html);
   const $ = await cheerio.load(html);
-  $("h2").each((index, element) => {
-    console.log($(element).text());
-  });
+  const red = $("#red").text()
+    console.log(red);
 }
 
 main();
