570a0c3ff04c67e554c4181796e822e24d137e98
Eric D. Torres <etorresnotary@gmail.com>
2020-11-24 08:25:33 -0600

    selecting with an html attribute

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const cheerio = require("cheerio");
 
 async function main() {
   const html = await request.get(
-    "https://reactnativetutorial.net/css-selectors/lesson5.html"
+    "https://reactnativetutorial.net/css-selectors/lesson6.html"
   );
 
   fs.writeFileSync("./test.html", html);
   const $ = await cheerio.load(html);
-  const red = $(".red").text()
+  const red = $('[data-customer=22293]').text()
     console.log(red);
 }
 
